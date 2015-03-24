093b7d89ad8a9c822f116687ab7760be103cfc40
jwight1234 <jwight1234@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-24 01:00:32 +0000

    commit tags try two
    git-svn-id: https://plugins.svn.wordpress.org/custom-upload-folders-plus/tags/1.0.1@1119302 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Uninstall Procedure for Custom Upload Folders
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) 
+	exit;
+
+
+// delete options
+delete_option( 'jwcuf_select' );
+delete_option( 'jwcuf_user_folder_name' );
+delete_option( 'jwcuf_default_folder_name' );
+delete_option( 'jwcuf_file_types' );
